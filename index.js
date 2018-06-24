31bda6d26387ce019771e1ac27c2f150d3b9cbce
Francis Tseng <f+accounts@frnsys.com>
2018-06-24 11:06:30 -0400

    basic audiometer

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function handleError(err) {
   toast.error(err.message);
 }
 
-// simple check
+// simple check, not particularly robust though
 function isDeviceId(id) {
   return typeof id === 'string';
 }
@@ -43,6 +43,57 @@ const DeviceSelect = (props) => {
       })}
     </select>
   </div>;
+}
+
+const sampleSize = 128**2;
+const sampleAvgInterval = 16;
+const meterTicks = 6;
+
+class AudioMeter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rms: 0
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    let stream = this.props.stream;
+    if (stream && stream !== prevProps.stream) {
+      // h/t tubertc
+      let audioContext = new AudioContext();
+      let mediaStreamSource = audioContext.createMediaStreamSource(stream);
+      let processor = audioContext.createScriptProcessor(sampleSize, 1, 1);
+      mediaStreamSource.connect(processor);
+      processor.connect(audioContext.destination);
+      processor.onaudioprocess = (ev) => {
+        var buf = ev.inputBuffer;
+        if (buf.numberOfChannels > 0) {
+          var inputData = buf.getChannelData(0);
+          var inputDataLength = inputData.length;
+          var total = 0;
+
+          for (var i = 0; i < inputDataLength; i += sampleAvgInterval) {
+            total += Math.abs(inputData[i]);
+          }
+
+          var rms = Math.sqrt((sampleAvgInterval * total) / inputDataLength);
+          this.setState({ rms });
+
+          // TODO broadcast if not muted
+        }
+      }
+    }
+  }
+
+  render() {
+    let nTicks = Math.round(this.state.rms*meterTicks);
+    return <div className='audiometer'>
+      {[...Array(nTicks).keys()].map((k) => {
+        return <span className='audiometer-tick' key={k}></span>
+      })}
+    </div>
+  }
 }
 
 class App extends Component {
@@ -121,9 +172,9 @@ class Preview extends Component {
   }
 
   render() {
-    return <div>
-      <h1>Hello Preview</h1>
+    return <div className='preview'>
       <video ref={this.video} autoPlay={true}></video>
+      <AudioMeter stream={this.state.stream} />
     </div>;
   }
 }
